10690fe32d8d65145d7dba3964a2edacb45536c5
Yuritalinda <yuritalinda@gmail.com>
2021-08-30 18:03:02 -0400

    cambios para deployment

--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@
 
 import { cambioRuta } from './controller/router.js';
 import { changeHash } from './controller/view-controller.js';
-import {portadaLogin} from './view/portada.js'
 
-
-
-
-document.getElementById('root').appendChild(portadaLogin);
 
  firebase.auth().onAuthStateChanged((user) => {
   if (user) {
